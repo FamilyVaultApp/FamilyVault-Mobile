ed48e93f71ff0dc9aabf996dcfcfba7168398d72
6ajmon <6ajmon@gmail.com>
2025-02-08 21:51:54 +0100

    Add FamilyGroupChooseScreen

--- composeApp/src/commonMain/kotlin/com/github/familyvault/screens/FamilyGroupChooseScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/screens/FamilyGroupChooseScreen.kt
@@ -2,20 +2,22 @@ package com.github.familyvault.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Cloud
+import androidx.compose.material.icons.outlined.GroupAdd
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Login
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.core.screen.Screen
 import com.github.familyvault.Constants
 import com.github.familyvault.components.AppIcon
 import com.github.familyvault.components.Button
@@ -29,11 +31,15 @@ import familyvault.composeapp.generated.resources.cloud_connection_mode_content
 import familyvault.composeapp.generated.resources.cloud_connection_mode_title
 import familyvault.composeapp.generated.resources.connection_modes_content
 import familyvault.composeapp.generated.resources.connection_modes_title
+import familyvault.composeapp.generated.resources.create_new_family_group_content
+import familyvault.composeapp.generated.resources.create_new_family_group_title
+import familyvault.composeapp.generated.resources.join_existing_family_group_content
+import familyvault.composeapp.generated.resources.join_existing_family_group_title
 import familyvault.composeapp.generated.resources.self_hosted_connection_mode_content
 import familyvault.composeapp.generated.resources.self_hosted_connection_mode_title
 import org.jetbrains.compose.resources.stringResource
 
-class InitialScreen : Screen {
+class FamilyGroupChooseScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
@@ -50,30 +56,29 @@ class InitialScreen : Screen {
             Headline1(stringResource(Res.string.app_name))
             Column(
                 modifier = Modifier.fillMaxHeight(),
-                verticalArrangement = Arrangement.spacedBy(Constants.mediumSpacing)
+                verticalArrangement = Arrangement.Bottom,
             ) {
-                InfoBox(
-                    title = stringResource(Res.string.connection_modes_title),
-                    content = stringResource(Res.string.connection_modes_content)
-                )
-                OptionButton(
-                    title = stringResource(Res.string.cloud_connection_mode_title),
-                    content = stringResource(Res.string.cloud_connection_mode_content),
-                    Icons.Outlined.Cloud,
-                    type = OptionButtonType.First
-                )
-                OptionButton(
-                    title = stringResource(Res.string.self_hosted_connection_mode_title),
-                    content = stringResource(Res.string.self_hosted_connection_mode_content),
-                    Icons.Outlined.Home,
-                    type = OptionButtonType.Second
-                )
-
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    OptionButton(
+                        title = stringResource(Res.string.create_new_family_group_title),
+                        content = stringResource(Res.string.create_new_family_group_content),
+                        Icons.Outlined.GroupAdd,
+                        type = OptionButtonType.First
+                    )
+                    OptionButton(
+                        title = stringResource(Res.string.join_existing_family_group_title),
+                        content = stringResource(Res.string.join_existing_family_group_content),
+                        Icons.Outlined.Login,
+                        type = OptionButtonType.Second
+                    )
+                }
                 Button(
                     "Dalej",
                     modifier = Modifier.fillMaxWidth(),
                     onClick = {
-                        navigator.push(FamilyGroupChooseScreen())
+                        navigator.push(InitialScreen())
                     }
                 )
             }
