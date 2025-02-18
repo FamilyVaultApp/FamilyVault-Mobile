63ef1860e67124f13092802be1c0d6166d4b6e89
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2025-02-19 00:21:47 +0100

    Remove unused import

--- composeApp/src/commonMain/kotlin/com/github/familyvault/components/typography/Headline1.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/components/typography/Headline1.kt
@@ -3,8 +3,6 @@ package com.github.familyvault.components.typography
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
