2f34b870a3ab5bacdc0537738abf8cc42760f652
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-02-27 22:27:01 +0100

    Adjust bottom navigation bar appearance

--- composeApp/src/commonMain/kotlin/com/github/familyvault/components/BottomNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/components/BottomNavigationBar.kt
@@ -1,5 +1,6 @@
 package com.github.familyvault.components
 
+import androidx.compose.foundation.border
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Chat
 import androidx.compose.material.icons.filled.Folder
@@ -8,18 +9,26 @@ import androidx.compose.material.icons.outlined.Chat
 import androidx.compose.material.icons.outlined.Folder
 import androidx.compose.material.icons.outlined.Task
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import com.github.familyvault.screens.FilesCabinetScreen
 import com.github.familyvault.screens.HomeScreen
 import com.github.familyvault.screens.TaskList
+import com.github.familyvault.ui.theme.AdditionalTheme
+
 
 
 @Composable
@@ -30,10 +39,17 @@ fun BottomNavigationBar(navigator: Navigator) {
         listOf(Icons.Outlined.Chat, Icons.Outlined.Task, Icons.Outlined.Folder)
     val items = listOf("Chat", "Tasks", "Cabinet")
     val screens = listOf(HomeScreen(), TaskList(), FilesCabinetScreen())
+    val NavigationBarDefaultColors = NavigationBarItemDefaults.colors().copy( selectedIconColor = AdditionalTheme.colors.firstOptionPrimaryColor, unselectedIconColor = MaterialTheme.colorScheme.onSurface )
 
-    NavigationBar {
+
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = AdditionalTheme.colors.firstOptionPrimaryColor,
+        modifier = Modifier.border(1.dp, AdditionalTheme.colors.borderColor, RectangleShape),
+            ) {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
+                colors = NavigationBarDefaultColors,
                 icon = {
                     Icon(
                         if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
