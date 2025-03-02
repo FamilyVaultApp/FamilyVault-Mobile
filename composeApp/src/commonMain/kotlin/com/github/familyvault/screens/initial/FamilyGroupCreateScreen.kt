99a3e9b85c944035d153b26cd3d355fd805ae5da
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2025-03-02 17:20:56 +0100

    Revert "Fix Custom Icon sizing"

--- composeApp/src/commonMain/kotlin/com/github/familyvault/screens/initial/FamilyGroupCreateScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/screens/initial/FamilyGroupCreateScreen.kt
@@ -1,17 +1,20 @@
 package com.github.familyvault.screens.initial
 
-import CustomIcon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -24,6 +27,7 @@ import com.github.familyvault.components.typography.Paragraph
 import com.github.familyvault.forms.FamilyGroupCreateForm
 import com.github.familyvault.ui.theme.AdditionalTheme
 import familyvault.composeapp.generated.resources.Res
+import familyvault.composeapp.generated.resources.app_icon_alt
 import familyvault.composeapp.generated.resources.create_new_family_group_title
 import familyvault.composeapp.generated.resources.family_group_create_screen_title
 import familyvault.composeapp.generated.resources.text_field_group_name_label
@@ -41,10 +45,7 @@ class FamilyGroupCreateScreen : Screen {
 
         StartScreen {
             CreateFamilyGroupHeader()
-            CustomIcon(
-                icon = Icons.Filled.Groups
-            )
-
+            UserProfilePicture()
             Column(
                 modifier = Modifier.fillMaxHeight(),
                 verticalArrangement = Arrangement.Bottom
@@ -68,6 +69,21 @@ class FamilyGroupCreateScreen : Screen {
             Headline1(
                 stringResource(Res.string.family_group_create_screen_title),
                 textAlign = TextAlign.Center,
+            )
+        }
+    }
+
+    @Composable
+    private fun UserProfilePicture() {
+        return Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                Icons.Filled.AccountCircle,
+                stringResource(Res.string.app_icon_alt),
+                modifier = Modifier.size(125.dp),
+                tint = AdditionalTheme.colors.firstOptionPrimaryColor
             )
         }
     }
