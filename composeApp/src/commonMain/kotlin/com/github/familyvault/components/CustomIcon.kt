e0e4f551c5e3e52d90f2d09ef2b6e658439f9b43
Adam Cedro <cedrq@tlen.pl>
2025-03-03 16:54:35 +0100

    Zmiana używanych kolorów

--- composeApp/src/commonMain/kotlin/com/github/familyvault/components/CustomIcon.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/components/CustomIcon.kt
@@ -4,13 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
-import com.github.familyvault.ui.theme.AdditionalTheme
 
 
 @Composable
@@ -26,14 +26,14 @@ fun CustomIcon(
         Box(
             modifier = Modifier
                 .size(140.dp)
-                .background(AdditionalTheme.colors.firstOptionSecondaryColor, CircleShape),
+                .background(MaterialTheme.colorScheme.primaryContainer, CircleShape),
             contentAlignment = Alignment.Center
         ) {
             Icon(
                 painter = rememberVectorPainter(icon),
                 contentDescription = contentDescription,
                 modifier = Modifier.size(85.dp),
-                tint = AdditionalTheme.colors.firstOptionPrimaryColor
+                tint = MaterialTheme.colorScheme.onPrimaryContainer
             )
         }
     }
