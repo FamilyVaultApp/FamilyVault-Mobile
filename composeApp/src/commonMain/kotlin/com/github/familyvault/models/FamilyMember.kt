7f9f2f83912d19e7d0de6b3dd97a60e4505c58ba
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-03-09 20:31:23 +0100

    Adjust FamilyMember class to backend implementation

--- composeApp/src/commonMain/kotlin/com/github/familyvault/models/FamilyMember.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/models/FamilyMember.kt
@@ -1,5 +1,6 @@
 package com.github.familyvault.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,8 +8,16 @@ data class FamilyMember(
     val firstname: String,
     val surname: String,
     val publicKey: String,
-    val permissionGroup: Int
+    val permissionGroup: FamilyGroupMemberPermissionGroup
 ){
     val fullname: String
         get() = "$firstname $surname"
+}
+
+
+@Serializable
+enum class FamilyGroupMemberPermissionGroup(val value: Int) {
+    @SerialName("0") Guardian(0),
+    @SerialName("1") Member(1),
+    @SerialName("2") Guest(2);
 }
