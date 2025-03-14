c62b2dff8b6ea745621488b766c380f8a38e4c4b
Filip Stefański <f.stefanski0322@gmail.com>
2025-03-14 16:00:19 +0100

    modified dialog for error handling

--- composeApp/src/commonMain/kotlin/com/github/familyvault/components/dialogs/FamilyGroupCreatingDialog.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/components/dialogs/FamilyGroupCreatingDialog.kt
@@ -18,37 +18,76 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.github.familyvault.components.overrides.Button
 import familyvault.composeapp.generated.resources.Res
+import familyvault.composeapp.generated.resources.creating_family_group_error
 import familyvault.composeapp.generated.resources.creating_family_group_label
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun FamilyGroupCreatingDialog() {
-    Dialog(
-        onDismissRequest = {}
-    ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp),
-            shape = RoundedCornerShape(16.dp),
+fun FamilyGroupCreatingDialog(
+    state: DialogState,
+    onDismiss: () -> Unit
+) {
+    if (state != DialogState.HIDDEN) {
+        Dialog(
+            onDismissRequest = {
+                if (state == DialogState.ERROR) {
+                    onDismiss()
+                }
+            }
         ) {
-            Column(
-                modifier = Modifier.fillMaxHeight().padding(16.dp),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                shape = RoundedCornerShape(16.dp),
             ) {
-                CircularProgressIndicator(
-                    modifier = Modifier.width(48.dp)
-                )
-                Text(
-                    text = stringResource(Res.string.creating_family_group_label),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentSize(Alignment.Center),
-                    textAlign = TextAlign.Center,
-                )
+                Column(
+                    modifier = Modifier.fillMaxHeight().padding(16.dp),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    when (state) {
+                        DialogState.LOADING -> {
+                            CircularProgressIndicator(
+                                modifier = Modifier.width(48.dp)
+                            )
+                            Text(
+                                text = stringResource(Res.string.creating_family_group_label),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentSize(Alignment.Center),
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+
+                        DialogState.ERROR -> {
+                            Text(
+                                text = stringResource(Res.string.creating_family_group_error),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentSize(Alignment.Center),
+                                textAlign = TextAlign.Center
+                            )
+                            Button(
+                                text = "Ok",
+                                onClick = onDismiss
+                            )
+                        }
+
+                        DialogState.HIDDEN -> {
+                            Unit
+                        }
+                    }
+                }
             }
         }
     }
+}
+
+enum class DialogState {
+    HIDDEN,
+    LOADING,
+    ERROR
 }
