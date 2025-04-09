e1c6fe02ff61b58da195f8d70678bb4068120cbd
Filip Stefański <f.stefanski0322@gmail.com>
2025-04-09 15:42:27 +0200

    add exception catching on qr code display

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/start/joinFamilyGroup/DisplayFamilyMemberDataQrCodeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/start/joinFamilyGroup/DisplayFamilyMemberDataQrCodeScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
@@ -14,6 +17,7 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.github.familyvault.services.IJoinStatusService
 import com.github.familyvault.states.IJoinFamilyGroupPayloadState
+import com.github.familyvault.ui.components.dialogs.ErrorDialog
 import com.github.familyvault.ui.components.typography.Headline3
 import com.github.familyvault.utils.IQrCodeGenerator
 import familyvault.composeapp.generated.resources.Res
@@ -21,8 +25,7 @@ import familyvault.composeapp.generated.resources.qr_code_generation_screen_cont
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 
-class DisplayFamilyMemberDataQrCodeScreen :
-    Screen {
+class DisplayFamilyMemberDataQrCodeScreen : Screen {
     @Composable
     override fun Content() {
         val joinFamilyGroupPayloadState = koinInject<IJoinFamilyGroupPayloadState>()
@@ -30,10 +33,29 @@ class DisplayFamilyMemberDataQrCodeScreen :
         val qrCodeGenerator = koinInject<IQrCodeGenerator>()
         val navigator = LocalNavigator.currentOrThrow
         val payload = remember { joinFamilyGroupPayloadState.getPayload() }
+        var error by remember { mutableStateOf(false) }
+
+        val codeBitmap = try {
+            qrCodeGenerator.generate(payload)
+        } catch (e: Exception) {
+            println(e)
+            error = true
+            null
+        }
 
         LaunchedEffect(Unit) {
-            joinTokenService.waitForNotInitiatedStatus(payload.joinStatusToken)
-            navigator.replaceAll(FamilyGroupJoinWaitingScreen())
+            try {
+                joinTokenService.waitForNotInitiatedStatus(payload.joinStatusToken)
+                navigator.replaceAll(FamilyGroupJoinWaitingScreen())
+            } catch (e: Exception) {
+                println(e)
+                error = true
+            }
+        }
+
+        if (error || codeBitmap == null) {
+            ErrorDialog { navigator.pop() }
+            return
         }
 
         Column(
@@ -43,7 +65,7 @@ class DisplayFamilyMemberDataQrCodeScreen :
         ) {
             Headline3(stringResource(Res.string.qr_code_generation_screen_content))
             Image(
-                bitmap = qrCodeGenerator.generate(payload),
+                bitmap = codeBitmap,
                 contentDescription = "QR Code"
             )
         }
