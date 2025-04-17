a8984f288deb3860f8ccc0884ee882d961346e21
Adam Cedro <cedrq@tlen.pl>
2025-04-17 20:48:41 +0200

    Fix password suggesting in FamilyGroupCreateAssignPrivateKeyPasswordScreen

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/components/overrides/TextField.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/components/overrides/TextField.kt
@@ -1,9 +1,11 @@
 package com.github.familyvault.ui.components.overrides
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import com.github.familyvault.ui.components.typography.Paragraph
@@ -26,6 +28,6 @@ fun TextField(
         supportingText = supportingText,
         onValueChange = onValueChange,
         singleLine = true,
-        visualTransformation = if (!isPassword) VisualTransformation.None else PasswordVisualTransformation(),
-    )
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,    )
 }
