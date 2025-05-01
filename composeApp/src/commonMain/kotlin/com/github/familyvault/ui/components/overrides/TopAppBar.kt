61993b1cc81616eef15d4f0de334e3a1e3bab8a8
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2025-05-02 01:21:52 +0200

    Fix surface color

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/components/overrides/TopAppBar.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/components/overrides/TopAppBar.kt
@@ -3,12 +3,14 @@ package com.github.familyvault.ui.components.overrides
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.github.familyvault.ui.components.typography.Paragraph
@@ -23,6 +25,9 @@ fun TopAppBar(
     actions: (@Composable () -> Unit)? = null
 ) {
     return MdTopAppBar(
+        modifier = Modifier.shadow(
+            AdditionalTheme.sizing.shadowSize,
+        ),
         title = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
