3b728827cfc34680b61035e75361faceab1bf6af
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-05-02 12:07:04 +0200

    Fix bugs

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/main/chat/GroupChatEdit.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/main/chat/GroupChatEdit.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
