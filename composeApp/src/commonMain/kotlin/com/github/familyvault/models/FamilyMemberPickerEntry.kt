cfacbc83317e10b2a142b7f01735080df462427a
6ajmon <6ajmon@gmail.com>
2025-05-20 12:28:05 +0200

    Change nobody to everyone in tasks assigning

--- composeApp/src/commonMain/kotlin/com/github/familyvault/models/FamilyMemberPickerEntry.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/models/FamilyMemberPickerEntry.kt
@@ -2,10 +2,10 @@ package com.github.familyvault.models
 
 import androidx.compose.runtime.Composable
 import familyvault.composeapp.generated.resources.Res
-import familyvault.composeapp.generated.resources.nobody
+import familyvault.composeapp.generated.resources.everyone
 import org.jetbrains.compose.resources.stringResource
 
 data class FamilyMemberPickerEntry(val familyMember: FamilyMember? = null) {
     @Composable
-    fun getName(): String = familyMember?.fullname ?: stringResource(Res.string.nobody)
+    fun getName(): String = familyMember?.fullname ?: stringResource(Res.string.everyone)
 }
