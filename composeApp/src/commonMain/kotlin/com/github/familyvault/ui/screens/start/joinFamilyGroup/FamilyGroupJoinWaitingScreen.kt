4b790989105ef33b864b0a760c63b1b457748e8b
Filip Stefański <f.stefanski0322@gmail.com>
2025-05-22 20:51:17 +0200

    add error dialog on join

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/start/joinFamilyGroup/FamilyGroupJoinWaitingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/start/joinFamilyGroup/FamilyGroupJoinWaitingScreen.kt
@@ -3,7 +3,10 @@ package com.github.familyvault.ui.screens.start.joinFamilyGroup
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -13,8 +16,10 @@ import com.github.familyvault.services.IFamilyMemberAdditionService
 import com.github.familyvault.services.IJoinStatusService
 import com.github.familyvault.states.IJoinFamilyGroupPayloadState
 import com.github.familyvault.ui.components.LoaderWithText
+import com.github.familyvault.ui.components.dialogs.ErrorDialog
 import com.github.familyvault.ui.components.screen.StartScreenScaffold
 import com.github.familyvault.ui.screens.main.MainScreen
+import com.github.familyvault.ui.screens.start.StartScreen
 import familyvault.composeapp.generated.resources.Res
 import familyvault.composeapp.generated.resources.loading
 import org.jetbrains.compose.resources.stringResource
@@ -30,28 +35,39 @@ class FamilyGroupJoinWaitingScreen : Screen {
         val joinFamilyGroupPayloadState = koinInject<IJoinFamilyGroupPayloadState>()
         val payload = remember { joinFamilyGroupPayloadState.getPayload() }
         val (identifier, keyPair) = payload.newMemberData
+        var joinError by remember { mutableStateOf(false) }
 
         LaunchedEffect(Unit) {
-            val joinStatusInfo = joinTokenService.waitAndGetJoinStatusInfo(payload.joinStatusToken)
+            try {
+                val joinStatusInfo =
+                    joinTokenService.waitAndGetJoinStatusInfo(payload.joinStatusToken)
 
-            if (joinStatusInfo.contextId == null) {
-                throw Exception("contextId from joinStatusInfo is null")
+                if (joinStatusInfo.contextId == null) {
+                    throw Exception("contextId from joinStatusInfo is null")
+                }
+
+                familyGroupService.joinFamilyGroupAndAssign(
+                    identifier.firstname,
+                    identifier.surname,
+                    joinFamilyGroupPayloadState.getEncryptedPrivateKey(),
+                    keyPair,
+                    joinStatusInfo.contextId
+                )
+                familyMemberAdditionService.afterJoinedToFamilyMembersOperations()
+
+                navigator.replaceAll(MainScreen())
+            } catch (e: Exception) {
+                joinError = true
             }
-
-            familyGroupService.joinFamilyGroupAndAssign(
-                identifier.firstname,
-                identifier.surname,
-                joinFamilyGroupPayloadState.getEncryptedPrivateKey(),
-                keyPair,
-                joinStatusInfo.contextId
-            )
-            familyMemberAdditionService.afterJoinedToFamilyMembersOperations()
-
-            navigator.replaceAll(MainScreen())
         }
 
 
         StartScreenScaffold {
+            if (joinError) {
+                ErrorDialog {
+                    navigator.replaceAll(StartScreen())
+                }
+            }
             LoaderWithText(
                 stringResource(Res.string.loading), modifier = Modifier.fillMaxSize()
             )
