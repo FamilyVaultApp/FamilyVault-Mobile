0529f8e516bad7c540a020a2cb529d8ce38670fd
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-05-23 14:39:12 +0200

    Add access denied header to match other similar components

--- composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/main/filesCabinet/FileCabinetPermissionDenied.kt
+++ composeApp/src/commonMain/kotlin/com/github/familyvault/ui/screens/main/filesCabinet/FileCabinetPermissionDenied.kt
@@ -10,13 +10,16 @@ import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.github.familyvault.ui.components.HeaderIcon
+import com.github.familyvault.ui.components.typography.Headline3
 import com.github.familyvault.ui.components.typography.ParagraphMuted
 import com.github.familyvault.ui.theme.AdditionalTheme
 import familyvault.composeapp.generated.resources.Res
 import familyvault.composeapp.generated.resources.file_cabinet_permission_denied
+import familyvault.composeapp.generated.resources.file_cabinet_permission_denied_header
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -29,6 +32,10 @@ fun FileCabinetPermissionDenied() {
             HeaderIcon(
                 Icons.Outlined.Warning,
                 size = AdditionalTheme.sizing.headerIconNormal
+            )
+            Headline3(
+                stringResource(Res.string.file_cabinet_permission_denied_header),
+                fontWeight = FontWeight.SemiBold
             )
             ParagraphMuted(
                 stringResource(Res.string.file_cabinet_permission_denied),
