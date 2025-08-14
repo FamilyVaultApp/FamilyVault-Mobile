2bcea72049e872438a460788734fb5b817b25045
Dawid Jenczewski <djenczewski@simplito.com>
2025-08-14 16:26:25 +0200

    feat(ios): Add qr code generation

--- composeApp/src/iosMain/kotlin/com/github/familyvault/utils/QrCodeGenerator.ios.kt
+++ composeApp/src/iosMain/kotlin/com/github/familyvault/utils/QrCodeGenerator.ios.kt
@@ -1,20 +1,43 @@
 package com.github.familyvault.utils
 
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import com.github.familyvault.models.AddFamilyMemberDataPayload
+import io.ktor.utils.io.core.toByteArray
+import kotlinx.cinterop.ExperimentalForeignApi
+import org.jetbrains.skia.Image
+import org.jetbrains.skia.makeFromEncoded
+import platform.CoreGraphics.CGAffineTransformMakeScale
+import platform.CoreImage.CIContext
+import platform.CoreImage.CIFilter
+import platform.CoreImage.PNGRepresentationOfImage
+import platform.CoreImage.QRCodeGenerator
+import platform.CoreImage.kCIFormatARGB8
+import platform.Foundation.setValue
+import kotlin.collections.emptyMap
 
 class QrCodeGenerator : IQrCodeGenerator {
     override fun generate(
         content: AddFamilyMemberDataPayload
     ): ImageBitmap {
-        TODO("Method not yet implemented")
+        return generateQrCodeBitmap(PayloadEncryptor.encrypt(content))
     }
 
     override fun generate(content: String): ImageBitmap {
-        TODO("Method not yet implemented")
+        return generateQrCodeBitmap(content)
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     private fun generateQrCodeBitmap(content: String): ImageBitmap {
-        TODO("Method not yet implemented")
+        val filter = CIFilter.QRCodeGenerator().apply {
+            setValue(content.toByteArray().asData(), forKey= "inputMessage")
+            setValue("L", forKey= "inputCorrectionLevel")
+        }
+        val context = CIContext()
+        val data = filter.outputImage?.let { outputImage ->
+            val scaledImage = outputImage.imageByApplyingTransform(CGAffineTransformMakeScale(5.0,5.0))
+            context.PNGRepresentationOfImage(scaledImage, format = kCIFormatARGB8, colorSpace = outputImage.colorSpace, options = emptyMap<Any?,Any>())
+        }
+        return Image.makeFromEncoded(data!!).toComposeImageBitmap()
     }
 }
