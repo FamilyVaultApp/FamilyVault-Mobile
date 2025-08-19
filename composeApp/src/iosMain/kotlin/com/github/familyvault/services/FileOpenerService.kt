b0825015423b6d46f3f24d24172687967da3df23
Doominika <dsluzynska@simplito.com>
2025-08-19 13:02:25 +0200

    feat: implement file opener service

--- composeApp/src/iosMain/kotlin/com/github/familyvault/services/FileOpenerService.kt
+++ composeApp/src/iosMain/kotlin/com/github/familyvault/services/FileOpenerService.kt
@@ -1,24 +1,109 @@
 package com.github.familyvault.services
 
-import com.github.familyvault.utils.FileTypeUtils
-import com.simplito.kotlin.privmx_endpoint.model.File
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.Foundation.NSApplicationSupportDirectory
+import platform.Foundation.NSData
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
+import platform.Foundation.dataWithBytes
+import platform.UIKit.UIApplication
+import platform.UIKit.UIDocumentInteractionController
+import platform.UIKit.UIDocumentInteractionControllerDelegateProtocol
+import platform.UIKit.UIViewController
+import platform.darwin.NSObject
 
 class FileOpenerService() : IFileOpenerService {
     private val TAG = "FileOpenerService"
-    
+    private val defaultManager = NSFileManager.defaultManager
+
+    @OptIn(ExperimentalForeignApi::class)
     override fun openFileWithExternalViewer(
-        fileBytes: ByteArray,
-        mimeType: String,
-        fileName: String
+        fileBytes: ByteArray, mimeType: String, fileName: String
     ): Boolean {
-        TODO("Method not yet implemented")
+        val fileUrl = createTempFile(fileBytes, fileName)
+        val fileNSUrl = NSURL.fileURLWithPath(fileUrl)
+
+        val documentInteractionController =
+            UIDocumentInteractionController.interactionControllerWithURL(fileNSUrl)
+        documentInteractionController.delegate = DocumentInteractionController(
+            UIApplication.sharedApplication.keyWindow?.rootViewController!!
+        )
+
+        return documentInteractionController.presentPreviewAnimated(true)
     }
-    
+
+    @OptIn(ExperimentalForeignApi::class)
     override fun downloadFile(fileBytes: ByteArray, fileName: String): String? {
-        TODO("Method not yet implemented")
+        val documentDirectory = defaultManager.URLForDirectory(
+            NSDocumentDirectory, NSUserDomainMask, null, true, null
+        )
+
+        val downloadsDirPath = documentDirectory?.URLByAppendingPathComponent("Downloads")
+        val filePath = downloadsDirPath?.URLByAppendingPathComponent(fileName)?.relativePath
+
+        if (!defaultManager.fileExistsAtPath(downloadsDirPath?.path!!)) {
+            defaultManager.createDirectoryAtURL(
+                downloadsDirPath,
+                withIntermediateDirectories = true,
+                attributes = null,
+                error = null
+            )
+        }
+        if (!defaultManager.fileExistsAtPath(filePath!!)) {
+            defaultManager.createFileAtPath(
+                path = filePath, contents = fileBytes.toNSData(), attributes = null
+            )
+        }
+        return filePath
     }
-    
-    private fun createTempFile(fileBytes: ByteArray, fileName: String): File {
-        TODO("Method not yet implemented")
+
+    @OptIn(ExperimentalForeignApi::class)
+    private fun createTempFile(fileBytes: ByteArray, fileName: String): String {
+        val applicationSupportDir = defaultManager.URLForDirectory(
+            NSApplicationSupportDirectory, NSUserDomainMask, null, true, null
+        )
+
+        val downloadsDirPath = applicationSupportDir?.URLByAppendingPathComponent("Cache")
+        val filePath = downloadsDirPath?.URLByAppendingPathComponent(fileName)?.relativePath
+
+        if (!defaultManager.fileExistsAtPath(downloadsDirPath?.path!!)) {
+            defaultManager.createDirectoryAtURL(
+                downloadsDirPath,
+                withIntermediateDirectories = true,
+                attributes = null,
+                error = null
+            )
+        }
+
+        if (!defaultManager.fileExistsAtPath(filePath!!)) {
+            defaultManager.createFileAtPath(
+                path = filePath, contents = fileBytes.toNSData(), attributes = null
+            )
+        }
+
+        return filePath
+    }
+}
+
+@OptIn(BetaInteropApi::class)
+class DocumentInteractionController(
+    val viewContoller: UIViewController
+) : NSObject(), UIDocumentInteractionControllerDelegateProtocol {
+    override fun documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController): UIViewController {
+        return viewContoller
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+fun ByteArray.toNSData(): NSData {
+    return usePinned {
+        NSData.dataWithBytes(
+            bytes = it.addressOf(0), size.toULong()
+        )
     }
 }
